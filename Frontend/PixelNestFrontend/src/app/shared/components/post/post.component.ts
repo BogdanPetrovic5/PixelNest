191ea2483eb4f551151a26796576c61b089d0879
Bogdan <petrovic215@gmail.com>
2024-09-11 15:16:24 +0200

    Frontend: Number of likes for each post is now displayed.

--- Frontend/PixelNestFrontend/src/app/shared/components/post/post.component.ts
+++ Frontend/PixelNestFrontend/src/app/shared/components/post/post.component.ts
@@ -13,8 +13,12 @@ import { UserSessionService } from 'src/app/core/services/user-session/user-sess
 })
 export class PostComponent implements OnInit{
   @Input() post:any
+  likedByUsers: { username: string }[] = [];
+
+
   formattedDate:string = ""
   username:string = ""
+
   isLiked:boolean | null = null
   constructor(
     private _datePipe:DatePipe,
@@ -25,29 +29,7 @@ export class PostComponent implements OnInit{
   }
 
   ngOnInit():void{
-    this.initilizeComponent();
-  }
-
-  initilizeComponent(){
-    this.username = this._userSession.getUsername();
-    this.formatDate()
-    this.checkIsLiked()
-  }
-  checkIsLiked(){
-    const foundUser = this.post.likedByUsers.find((user:any) => user.username === this.username);
-    if(foundUser) {
-      this.isLiked = true
-    }else this.isLiked = false
-  }
-  formatDate(){
-    const date = this.post.publishDate;
-      const dateObject = new Date(date)
-
-      if (isNaN(dateObject.getTime())) {
-        this.formattedDate = 'Invalid Date';
-      } else {
-        this.formattedDate = this._datePipe.transform(dateObject, 'd MMM \'at\' HH:mm') || 'Invalid Format';
-      }
+    this._initilizeComponent();
   }
 
   likePost(postID:number){
@@ -55,7 +37,8 @@ export class PostComponent implements OnInit{
     
     this._postService.likePost(postID, this.username).pipe(
       tap((response)=>{
-        this.isLiked = !this.isLiked
+        this._handleLikeArray();
+        
         console.log("Successfully liked the post")
       }),
       catchError((error:HttpErrorResponse)=>{
@@ -66,5 +49,38 @@ export class PostComponent implements OnInit{
         console.log("End")
       })
     ).subscribe()
+  }
+
+  private _initilizeComponent(){
+    this.username = this._userSession.getUsername();
+    this._formatDate()
+    this._checkIsLiked()
+  }
+
+  private _checkIsLiked(){
+    const foundUser = this.post.likedByUsers.find((user:any) => user.username === this.username);
+    this.likedByUsers = this.post.likedByUsers;
+    if(foundUser) {
+      this.isLiked = true
+    }else this.isLiked = false
+  }
+
+  private _formatDate(){
+    const date = this.post.publishDate;
+      const dateObject = new Date(date)
+
+      if (isNaN(dateObject.getTime())) {
+        this.formattedDate = 'Invalid Date';
+      } else {
+        this.formattedDate = this._datePipe.transform(dateObject, 'd MMM \'at\' HH:mm') || 'Invalid Format';
+      }
+  }
+
+  
+  private _handleLikeArray(){
+    if(!this.isLiked) this.likedByUsers.push({username: this.username})
+    else this.likedByUsers = this.likedByUsers.filter(user => user.username !== this.username)
+    this.isLiked = !this.isLiked
+
   }
 }
