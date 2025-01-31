7c133389180d10214a7547bbd67292bb99eef60e
Bogdan <94244233+BogdanPetrovic5@users.noreply.github.com>
2025-01-31 10:25:02 +0100

    Backend: Additional file

---
+++ Backend/PixelNestBackend/PixelNestBackend/Controllers/NotificationController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PixelNestBackend.Dto;
+using PixelNestBackend.Dto.Projections;
+using PixelNestBackend.Interfaces;
+using System.Security.Claims;
+
+namespace PixelNestBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationController(INotificationService notificationService)
+        {
+
+            _notificationService = notificationService;
+
+        }
+        [Authorize]
+        [HttpPost("MarkAsOpened")]
+        public ActionResult<bool> MarkAsOpened([FromBody]MarkAsOpenedDto markAsRead)
+        {
+            string? email = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (email == null) return Unauthorized();
+
+            bool result = _notificationService.MarkAsRead(markAsRead, email);
+            return Ok(result);
+        }
+        [Authorize]
+        [HttpGet("CountNotifications")]
+        public ActionResult<int> CountNotifications()
+        {
+            string? email = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (email == null) return Unauthorized();
+
+            int notifications = _notificationService.CountNotifications(email);
+            return Ok(notifications);
+        }
+        [Authorize]
+        [HttpGet("GetAllNotifications")]
+        public ActionResult<ICollection<ResponseNotificationsDto>> GetAllNotifications()
+        {
+            string? email = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (email == null) return Unauthorized();
+
+            ICollection<ResponseNotificationsDto> result = _notificationService.GetNotifications(email);
+           
+            if (result != null) return Ok(result);
+            else return NotFound();
+
+        }
+    }
+}
