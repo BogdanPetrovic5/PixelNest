100d2f82080aad6252c6ba9fc553bd8d9a443221
Bogdan <94244233+BogdanPetrovic5@users.noreply.github.com>
2025-05-24 12:37:00 +0200

    Frontend: Updated URLs to correspond to changes made on backend

--- Frontend/PixelNestFrontend/src/app/core/services/user/user.service.ts
+++ Frontend/PixelNestFrontend/src/app/core/services/user/user.service.ts
@@ -14,63 +14,58 @@ export class UserService {
 
   constructor(private _httpClient:HttpClient) { }
   updateLocation(location:any):Observable<any>{
-    const url = `${environment.apiUrl}/api/User/ShareLocation`
+    const url = `${environment.apiUrl}/api/user/location`
     return this._httpClient.patch(url, location);
   }
   getUserData(clientGuid:string):Observable<any>{
-    const url = `${environment.apiUrl}/api/User/GetUserProfile?clientGuid=${clientGuid}`
+    const url = `${environment.apiUrl}/api/user/users/${clientGuid}`
     return this._httpClient.get(url);
   }
 
   getFollowers(clientGuid:string):Observable<FollowersDto[]>{
-    const url = `${environment.apiUrl}/api/User/GetFollowers?clientGuid=${clientGuid}`
+    const url = `${environment.apiUrl}/api/user/${clientGuid}/followers`
     return this._httpClient.get<FollowersDto[]>(url);
   }
 
   getFollowings(clientGuid:string):Observable<FollowingsDto[]>{
-    const url = `${environment.apiUrl}/api/User/GetFollowings?clientGuid=${clientGuid}`
+    const url = `${environment.apiUrl}/api/user/${clientGuid}/followings`
     return this._httpClient.get<FollowingsDto[]>(url);
   }
 
   isFollowing(targetClientGuid:string):Observable<boolean>{
-    const url = `${environment.apiUrl}/api/User/IsFollowing?targetClientGuid=${targetClientGuid}`
+    const url = `${environment.apiUrl}/api/user/followings/${targetClientGuid}`
     return this._httpClient.get<boolean>(url);
   }
 
   follow(targetClientGuid:string):Observable<any>{
-    const url = `${environment.apiUrl}/api/User/Follow?targetClientGuid=${targetClientGuid}`
+    const url = `${environment.apiUrl}/api/user/follow/${targetClientGuid}`
     return this._httpClient.post<any>(url,{})
   }
 
-  validateUser(postOwner:string):Observable<boolean>{
-    const url = `${environment.apiUrl}/api/User/IsValid?postOwner=${postOwner}`
-
-    return this._httpClient.get<boolean>(url);
-  }
 
   changeProfilePicture(formData:FormData):Observable<boolean>{
-    const url = `${environment.apiUrl}/api/User/ChangeProfilePicture`
+    const url = `${environment.apiUrl}/api/user/profile-picture`
 
     return this._httpClient.put<boolean>(url,formData);
   }
   changeUsername(formData:FormData):Observable<boolean>{
-    const url = `${environment.apiUrl}/api/User/ChangeUsername`
+    const url = `${environment.apiUrl}/api/user/username`
     return this._httpClient.put<boolean>(url,formData)
   }
   
   getProfilePicture(clientGuid:string):Observable<any>{
-    const url = `${environment.apiUrl}/api/User/GetProfilePicture?clientGuid=${clientGuid}`
+    const url = `${environment.apiUrl}/api/user/profile-picture/${clientGuid}`
 
     return this._httpClient.get<any>(url);
   }
   findUsers(username:string):Observable<Users[]>{
-    const url = `${environment.apiUrl}/api/User/FindUsers?username=${username}`
+    const url = `${environment.apiUrl}/api/user/search?username=${username}`
 
     return this._httpClient.get<Users[]>(url);
   }
 
   getCurrentUserData(){
-    const url = `${environment.apiUrl}/api/User/GetCurrentUserData`
+    const url = `${environment.apiUrl}/api/user/me`
     return this._httpClient.get<any>(url);
   }
 
